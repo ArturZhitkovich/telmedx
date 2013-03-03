3c1c763d748d5d18589e605b2d7ad6aac15a4a1c
Tereus Scott <tereus@tereus.net>
2013-03-03 11:53:49 -0800

    refactored main video view and moved static javascript and css into standalone files

---
+++ static/js/viewmaster.js
@@ -0,0 +1,81 @@
+// User Interface Elements
+
+		function inviteClicked() {
+			jQuery.post("/ttux/invite");
+		}
+		
+		function select(id) {
+			$(".snapshot").css("border", "none");
+			$("#" + id).css("border", "4px solid");
+		}
+		
+		function showSnapshot(id) {
+			var selected = $("#" + id);
+			
+			$("#activeSnapshot").attr("src", selected.attr("src"));
+			select(id);
+		}
+		
+		function takeSnapshotClicked(deviceName) {
+			$.post("/ttux/snapshot/" + deviceName, null, function(data) {
+				dataUri = "data:image/jpeg;base64," + data.image;
+				$("#activeSnapshot").attr("src", dataUri);
+				
+				var id = "snapshot-" + new Date().getTime().toString();
+				var imageElement = "<img class=\"snapshot\" id=\"" + id + "\" src=\"" + dataUri + "\" width=\"75\" height=\"50\" onclick=\"window.parent.showSnapshot('" + id + "')\"> "
+				
+				$("#pastSnapshots").append(imageElement);
+				
+				select(id);
+			});
+			
+			$("#activeSnapshot").attr("src", "/static/img/spinner.gif")
+		}
+
+		function saveCurrentImage() {
+			console.log("save image selected");
+		}
+
+		function printCurrentImage() {
+			console.log("print image selected");
+		}
+
+
+		var ff=0;
+		var lastFrame = 0;
+		var fCounter = 0;
+		var startTime = new Date();
+
+		// jquery test
+		function frameOne_jq(deviceName)
+		{
+			var r = $.ajax({
+				url: "/ttux/lastFrame/" + deviceName + "/" + lastFrame
+			});
+			
+			r.done( function(msg) {
+				var fnumber = msg.substring(0,8);
+				lastFrame = fnumber;
+
+				if (msg.substring(8).length > 0) {
+	            			videoSrc = "data:image/jpg;base64," + msg.substring(8);
+	            			$("#hollywood").attr("src", videoSrc);
+	            			var now = new Date();
+	            			var min = Math.floor( ( (now - startTime)/1000) );
+	            			var sec = (now - startTime)%1000;
+	            			$("#stream_status").html("{{ dev.name }}: Connected:" + min + "." + sec + " f:" + fCounter);
+	            			fCounter++;
+				}
+	            
+				//console.log("got frame: " + fnumber);
+				//setTimeout("frameOne_jq()", 100);
+				frameOne_jq(deviceName);
+			});
+
+			// error handler, wait 1/2 second
+			r.error( function(msg) {
+				console.log("got error");
+				setTimeout("frameOne_jq(" + deviceName + ")", 500);
+			});
+
+		}		
