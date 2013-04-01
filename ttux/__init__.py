27679b1173150f93ff1679b3b818ed57defe9260
Tereus Scott <tereus@tereus.net>
2013-04-01 11:52:55 -0700

    removed session initialization code. This is not needed anymore. We will now create sessions on the fly.

--- ttux/__init__.py
+++ ttux/__init__.py
@@ -18,14 +18,16 @@ from ttux.session import Session
 #logging.basicConfig(level=logging.DEBUG)
 
 print("ttux init: enter")
-#logger.info("Enter")
-try:
-    deviceList = mobileCam.objects.all().order_by('name')[:4]
-    for d in deviceList:
-        print "Creating session for device:" + d.name
-        Session.put(d.name, Session())
-except:
-    print("initialization failure, creating db?")
+##logger.info("Enter")
+#try:
+#    deviceList = mobileCam.objects.all().order_by('name')[:4]
+#    for d in deviceList:
+#        print "Creating session for device:" + d.name
+#        #TODO for testing, remove this. This will normally create a session object for each device
+#        # in the database
+#        #Session.put(d.name, Session())
+#except:
+#    print("initialization failure, creating db?")
 
 #logger.info("Exit")
 print("ttux init: exit")
