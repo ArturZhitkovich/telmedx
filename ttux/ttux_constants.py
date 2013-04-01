826967ba4aa744a33bb3058f72662126b2a8dfc4
Tereus Scott <tereus@tereus.net>
2013-04-01 12:01:14 -0700

    adding constants file for data used by the session object

---
+++ ttux/ttux_constants.py
@@ -0,0 +1,19 @@
+'''
+Created on 2013-03-31
+
+@author: developer
+'''
+# RETURN CODES
+RC_BAD_SUID         = "BAD_SUID"
+RC_REGISTER_OK      = "REGISTER_OK"
+RC_REGISTER_FAIL    = "REGISTER_FAIL"
+
+RC_SESSION_OK       = "SESSION_OK"
+RC_SESSION_FAIL     = "SESSION_FAIL"
+
+# HTTP STATUS CODES
+HSTAT_OK            = 200
+HSTAT_AUTH_FAIL     = 418
+#HSTAT_BAD_DEVICE    = 418   # HTTP status code returned when the device SUID does not exist in the REGISTRY
+HSTAT_LOGIN_SUCCESS = 302   # HTTP status coder returned after successful login to django
+HSTAT_NO_KEYS_LEFT  = 418   # server ran out of Tickets
