85856e32986d76288b16e407da1e85aafc49f87f
Brian <laxnski@gmail.com>
2014-10-20 23:34:30 -0700

    Makes header smaller and names export properly

--- usage/utils.py
+++ usage/utils.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 import csv
+import datetime
 
 def export_usage_csv(logs):
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+    response['Content-Disposition'] = 'attachment; filename="'+datetime.datetime.now().strftime('%Y-%m-%d')+'-usage.csv"'
 
     writer = csv.writer(response)
     writer.writerow(['Device','Date and Time','Duration','Frames Sent','Images Captured'])
