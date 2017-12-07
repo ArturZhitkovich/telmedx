26b7c12b0311aef5fe695bdb827974a9294d51e2
AlexanderV <avaughan@barefootsolutions.com>
2017-12-06 17:30:30 -0800

    Local Environment Settings

---
+++ telmedx/unfinished_start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Activate source via command line:
+# activate_shell
+
+# Do NOT currently run this script:
+# Need to have seperate shells running each of these
+./local_restart.sh &
+./local_brunch.sh
