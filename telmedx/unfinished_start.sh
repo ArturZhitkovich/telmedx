b4fd9f8b6f42423114ff4e0e2b577ef8aef62cd8
AlexanderV <avaughan@barefootsolutions.com>
2017-12-07 17:25:14 -0800

    Update git ignore

--- telmedx/unfinished_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Activate source via command line:
-# activate_shell
-
-# Do NOT currently run this script:
-# Need to have seperate shells running each of these
-./local_restart.sh &
-./local_brunch.sh
