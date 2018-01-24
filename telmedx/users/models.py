b1923d5c0679de5f456a9c1397041fe18d111127
Allan Almazan <allan.almazan@gmail.com>
2018-01-24 11:58:04 -0800

    Add uuid to users

--- telmedx/users/models.py
+++ telmedx/users/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser, UserManager, Group
 from django.db import models
 from django.db.models.signals import post_save
@@ -67,6 +69,7 @@ class TelmedxProfile(models.Model):
 
 class TelmedxUser(AbstractUser):
     objects = TelmedxUserManager()
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
     @property
     def date_created(self):
