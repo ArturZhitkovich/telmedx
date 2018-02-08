80c01448553eec4e21446737a8d76e2acf243b90
Allan Almazan <allan.almazan@gmail.com>
2018-02-08 13:11:48 -0800

    Change username == email, login form changes

--- telmedx/users/models.py
+++ telmedx/users/models.py
@@ -1,8 +1,9 @@
 import uuid
 
 from django.contrib.auth.models import AbstractUser, UserManager, Group
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 USER_GLOBAL_ADMIN = 1
@@ -79,8 +80,12 @@ class TelmedxProfile(models.Model):
 
 
 class TelmedxUser(AbstractUser):
-    objects = TelmedxUserManager()
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    email = models.EmailField(_('email address'), blank=True, unique=True)
+
+    objects = TelmedxUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     @property
     def date_created(self):
@@ -98,10 +103,10 @@ class TelmedxUser(AbstractUser):
 
 
 # Uncomment if we want to have email == username
-# @receiver(pre_save, sender=TelmedxUser)
-# def populate_username(sender, instance, **kwargs):
-#     instance.username = instance.email
-#     return instance
+@receiver(pre_save, sender=TelmedxUser)
+def populate_username(sender, instance, **kwargs):
+    instance.username = instance.email
+    return instance
 
 
 @receiver(post_save, sender=TelmedxUser)
