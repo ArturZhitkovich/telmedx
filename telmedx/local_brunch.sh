dc48234feb82f5caac389246fa16b6dd300a61e5
AlexanderV <avaughan@barefootsolutions.com>
2018-07-23 13:36:58 -0700

    Add scripts directory

--- telmedx/local_brunch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source bin/activate
-cd tel_static/brunch
-sudo brunch w
-
-
--- telmedx/local_restart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source bin/activate
-./manage.py collectstatic --noinput
-./manage.py runserver 10.0.0.96:8000
