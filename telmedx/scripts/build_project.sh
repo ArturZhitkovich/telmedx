961662fd2ee63cc9935acf0fa80b29ae91341ed6
Telmedx <admin@telmedx.com>
2018-07-23 20:44:05 +0000

    Update build script

--- telmedx/scripts/build_project.sh
+++ telmedx/scripts/build_project.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-source ../bin/activate
+source ../../bin/activate
 cd ../tel_static/brunch
 sudo brunch build
 cd ../../
 ./manage.py collectstatic --noinput
+
+echo " "
+echo "To get the status of the app: sudo supervisorctl status"
+echo "To restart the instance: sudo supervisorctl restart telx"
+
